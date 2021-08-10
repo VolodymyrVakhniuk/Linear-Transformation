b65fc20b98a089a7c538f8131bf5eb3f93a50def
nick_parovoy <32142855+nickparov@users.noreply.github.com>
2021-08-10 14:02:13 -0500

    + small update

---
+++ export.js
@@ -0,0 +1,9 @@
+export { colorPalette } from "./Renderer/Color.js";
+export { GridRender } from "./Renderer/GridRenderer.js";
+export { MainCanvas } from "./Renderer/MainCanvas.js";
+export { OriginRenderer } from "./Renderer/OriginRenderer.js";
+export { Vector2DRenderer } from "./Renderer/Vector2DRenderer.js";
+export { LinearTransformation } from "./Math/LinearTransformation.js";
+export { LTAnimation } from './Animation.js';
+
+
